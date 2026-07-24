d367a724b1e2ebfd99dd7ea277bce0b15a177651
eto <eto.saviour80@gmail.com>
2026-07-24 10:06:06 +0100

    update

---
+++ routes/web.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home.homepage');
+});
+
+Route::get('/contact', function () {
+    return view('home.contact');
+});
+
+Route::get('/education', function () {
+    return view('home.education');
+});
+
+Route::get('/cryptocurrencies', function () {
+    return view('home.cryptocurrencies');
+});
+
+Route::get('/forex', function () {
+    return view('home.forex');
+});
+
+Route::get('/shares', function () {
+    return view('home.shares');
+});
+
+Route::get('/indices', function () {
+    return view('home.indices');
+});
+
+Route::get('/etfs', function () {
+    return view('home.etfs');
+});
+
+
+
+Route::get('/trade', function () {
+    return view('home.trade');
+});
+
+Route::get('/copy-trading', function () {
+    return view('home.copy-trading');
+});
+
+Route::get('/automated-trading', function () {
+    return view('home.automated-trading');
+});
+
+Route::get('/about', function () {
+    return view('home.about');
+});
+
+Route::get('/why-us', function () {
+    return view('home.why-us');
+});
+
+Route::get('/faq', function () {
+    return view('home.faq');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+});
+
+Route::get('/terms-and-conditions', function () {
+    return view('home.terms-and-conditions');
+});
+
+Route::get('/privacy-policy', function () {
+    return view('home.privacy-policy');
+});
